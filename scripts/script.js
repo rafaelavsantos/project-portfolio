bf2c76a682d689211d1863e16a1b90511a44b8e3
Rafaela Santos <rafaelasantosminios@gmail.com>
2025-12-27 16:36:57 -0300

    feat: problema no link vazio para trocar o tema

--- scripts/script.js
+++ scripts/script.js
@@ -27,7 +27,12 @@ const navLinks = document.querySelectorAll('#menu-container ul a.links-menu');
 navLinks.forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
+    const href = this.getAttribute('href');
+
+    if(!href || href === '#') return;
+
+    const target = document.querySelector(href);
+    
     if (target) {
       const headerHeight = document.querySelector('header').offsetHeight;
       const targetPosition = target.offsetTop - headerHeight - 60;
